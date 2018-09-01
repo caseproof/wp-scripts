910b716250d49f5822dacf368ec73f12dca34569
Brandon Toone <brandon@rdlk.co>
2018-09-01 12:13:47 -0600

    Add main file definition

--- wp-script-config.sample.php
+++ wp-script-config.sample.php
@@ -2,6 +2,7 @@
 if(!defined('STDIN')) { die("You're unauthorized to view this page."); }
 
 define('WP_SCRIPT_TEXTDOMAIN','');
+define('MY_PLUGIN_MAIN_FILE', 'main.php');
 define('MOTHERSHIP_URL','');
 define('MOTHERSHIP_API_KEY','');
 define('MOTHERSHIP_PROJECT_ID','');
